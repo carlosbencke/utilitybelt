065001f92d3a767bff7437c87c943d15b99bdf68
carlos <carlos@pop-os.localdomain>
2023-10-24 20:52:48 -0300

    Adicionada a função para ordenação em ordem alfabética

--- text_manipulation.py
+++ text_manipulation.py
@@ -1,8 +1,39 @@
 #! /usr/bin/python3
 
-text = "I'm Batman!"
 
+text = """This is a sample text.
+And this is another line.
+I'm batman!"""
+
+# Inverte o texto
 def text_invert(text):
     return text[::-1]
 
-print(text_invert(text))
+# Caixas altas
+def uppercase(text):
+	return text.upper()
+
+# Caixa baixa
+def lowercase(text):
+	return text.lower()
+
+# Primeira letra em maiúsculo
+def first_upper(text):
+	return text.title()
+
+#Remove  linhas iguais
+def remove_duplicated_lines(text):
+	raw_list = text.splitlines()
+	print(raw_list)
+	coocked_list = []
+
+	for line in raw_list:
+		if line not in coocked_list:
+			coocked_list.append(line)
+	return str(coocked_list)
+
+# Coloca em ordem alfabética
+def ordem_alfabetica(text):
+	raw_list = text.splitlines()
+	raw_list.sort()
+	return raw_list
